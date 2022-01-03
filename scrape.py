9687efe2137270d20aabc72a77825dc5fafadade
Zsolt Kovari <zsolt@kovaridev.com>
2022-01-03 13:07:09 +0100

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,43 @@
+import json
+import re
+import time
+
+from atomicwrites import atomic_write
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://www.artbreeder.com/browse")
+elem = driver.find_element_by_xpath("//*[@data-name='portraits_sg2']")
+elem.click()
+
+SCROLL_PAUSE_TIME = 1
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+i = 0
+while i < 50:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+    i += 1
+
+urls = []
+elements = driver.find_elements_by_class_name('main_image')
+for el in elements:
+    match = re.search(r'http.*\.(jpeg|png|jpg|gif)', el.get_attribute('style'))
+    if match:
+        urls.append(match.group(0))
+
+driver.close()
+
+with atomic_write('resources/artbreeder/portraits.json', overwrite=True) as f:
+    f.write(json.dumps(urls))
